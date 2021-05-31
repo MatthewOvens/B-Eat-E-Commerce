e51b7d1b7f1738a56fdc4ce14a4265732e86cc11
Team Blue <elvis_dany_c@hotmai.it>
2021-05-31 21:09:22 +0200

    funga

--- src/app/page-one/page-one.component.ts
+++ src/app/page-one/page-one.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import food from '../files/food.json';
 @Component({
   selector: 'app-page-one',
   templateUrl: './page-one.component.html',
   styleUrls: ['./page-one.component.scss']
 })
 export class PageOneComponent implements OnInit {
+  
   filter_value = '';
   constructor() { }
 
@@ -16,4 +17,9 @@ export class PageOneComponent implements OnInit {
     console.log(value);
     this.filter_value = value;
   }
+
+  foodList:{id:string, nome:string, tipo:string
+  cucina:string, mediaValutazione:string
+  numRecensioni:string, consegna:string
+  indirizzo:string, img1:string, img2:string}[] = food;
 }
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,24 +1,24 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import food from '../files/food.json';
+
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
-  
+  filter_value = '';
   constructor() { }
-  foodList:{id:string, nome:string, tipo:string
-          cucina:string, mediaValutazione:string
-          numRecensioni:string, consegna:string
-          indirizzo:string, img1:string, img2:string}[] = food;
+  
   ngOnInit(): void {
     this.search.valueChanges.subscribe(value => this.searchEmitter.emit(value));
   }
 
   search = new FormControl('');
-
+handleSearch(value:string){
+    console.log(value);
+    this.filter_value = value;
+  }
 
   @Output('search') searchEmitter = new EventEmitter<string>();
 }
